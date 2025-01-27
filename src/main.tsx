4058bfd980eeb3b1083c7f20748ffe68e5b160f5
Naylek <kelyan.desmet1@gmail.com>
2025-01-27 22:28:45 +0100

    Media query for screen size, Footer on main.tsx, Home text changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import Home from './components/Home.tsx'
+import Footer from "./components/Footer.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+    <StrictMode>
+        <Home/>
+        <Footer/>
+    </StrictMode>,
 )
